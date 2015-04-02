c3027c5745b1baff31e7a8b7e2c1c0b49210703c
Nany69 <t.issop69@gmail.com>
2015-04-02 13:53:03 +0200

    echehance style of application

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,10 +9,13 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( 
+Rails.application.config.assets.precompile += %w(
   teaspoon.css
   teaspoon-teaspoon.js
   teaspoon-jasmine.js
 )
+
+Rails.application.config.assets.precompile += %w( bootstrap/glyphicons-halflings-regular.woff2 )
+
 
 Rails.application.assets.register_engine '.haml', Tilt::HamlTemplate
